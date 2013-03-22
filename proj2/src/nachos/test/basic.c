ed67c8a6a041eee532ade1ca9ba88fc7775bd499
Class_Account <baugarten@gmail.com>
2013-03-22 16:05:04 -0700

    Added test for larger file IO

--- proj2/src/nachos/test/basic.c
+++ proj2/src/nachos/test/basic.c
@@ -8,6 +8,7 @@ int main(int argc, char* argv) {
 	int amt_written, closed, unlinked;
 	char buf[50];
 	char empty[50];
+	char buf2[2050];
 	int amount_read = read(descriptor, buf, 20);
 	printf("%d should be %d\n", amount_read, 0);
 	strcpy(buf,"C programming is not for everyone");
@@ -33,4 +34,20 @@ int main(int argc, char* argv) {
 	descriptor = open("tmp.txt");
 	printf("%d should be %d\n", descriptor, -1);
 	printf("%s\n", buf);
+
+	amount_read = read(4, buf, 50);
+	printf("%d should be %d\n", descriptor, -1);
+	amt_written = write(4, buf, 50);
+	printf("%d should be %d\n", descriptor, -1);
+
+	descriptor = open("random.txt");
+	amount_read = read(descriptor, buf2, 2048);
+	printf("%d should be %d\n", amount_read, 2048);
+	amount_read = read(descriptor, buf2, 2049);
+	printf("%d should be %d\n", amount_read, 2049);
+	amount_read = read(descriptor, buf2, 2047);
+	printf("%d should be %d\n", amount_read, 2047);
+	amount_read = read(descriptor, buf2, 10050); // should hit a read only section
+	printf("%d should be %d\n", amount_read, -1);
+
 }
