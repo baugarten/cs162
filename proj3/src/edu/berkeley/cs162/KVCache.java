f3f2106bd37bd1869521f544284467124610f9b2
steventsim <steventsim@gmail.com>
2013-04-12 20:06:43 -0700

    fix package

--- proj3/src/edu/berkeley/cs162/KVCache.java
+++ proj3/src/edu/berkeley/cs162/KVCache.java
@@ -28,7 +28,7 @@
  *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package src.edu.berkeley.cs162;
+package edu.berkeley.cs162;
 
 import java.util.HashMap;
 import java.util.LinkedList;
