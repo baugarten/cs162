7cf46983646787d26a62e5301d33269eecf6a980
Minh Phan <mqphan@berkeley.edu>
2013-04-17 10:19:51 -0700

    update the tryout branch

--- proj3/src/edu/berkeley/cs162/Client.java
+++ proj3/src/edu/berkeley/cs162/Client.java
@@ -45,11 +45,11 @@ public class Client {
 			String three = "3";
 			String seven = "7";
 			System.out.println("putting (3, 7)");
-			boolean status = kc.put(three, seven);
+		  	kc.put(three, seven);
 			System.out.println("status: " + status);
 
 			System.out.println("putting (3, 7) (again)");
-			status = kc.put(three, seven);
+			kc.put(three, seven);
 			System.out.println("status: " + status);
 			
 			System.out.println("getting key=3");			
